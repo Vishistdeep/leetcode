ec221cbfac2a9f52a7cbbb2f1a630f0db8b38651
Vishistdeep <162116879+Vishistdeep@users.noreply.github.com>
2024-03-22 08:20:52 +0530

    Time: 4 ms (50.28%) | Memory: 7.5 MB (91.15%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -12,7 +12,9 @@ public:
             dp[0][i] = false;
         }
         for(int i = 1; i <= m; i++){
-            if(i > 1 && p[i-1] == '*') dp[0][i] |= dp[0][i-2];
+            if(i > 1 && p[i-1] == '*') {
+                dp[0][i] |= dp[0][i-2];
+            }
         }
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
@@ -21,7 +23,9 @@ public:
                 }
                 else if(p[j-1] == '*'){
                     if(j >= 2){
-                        if(s[i-1] == p[j-2] || p[j-2] == '.') dp[i][j] = dp[i-1][j];
+                        if(s[i-1] == p[j-2] || p[j-2] == '.'){
+                             dp[i][j] = dp[i-1][j];
+                        }
                         else dp[i][j] = false;
                     }
                     else dp[i][j] = false;
