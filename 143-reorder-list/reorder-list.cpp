c05ac5da69abc72f367fbf1b00c48bdafb17e6e0
Vishistdeep <162116879+Vishistdeep@users.noreply.github.com>
2024-03-23 13:05:54 +0530

    Time: 23 ms (69.67%) | Memory: 21.1 MB (90.75%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -12,16 +12,12 @@ class Solution {
 public:
     void reorderList(ListNode* head) {
        if (head == nullptr || head->next == nullptr) return;
-
-        // Step 1: Find the middle of the list
         ListNode* slow = head;
         ListNode* fast = head;
         while (fast != nullptr && fast->next != nullptr) {
             slow = slow->next;
             fast = fast->next->next;
         }
-
-        // Step 2: Reverse the second half of the list
         ListNode* prev = nullptr;
         ListNode* next = nullptr;
         while (slow != nullptr) {
@@ -30,10 +26,8 @@ public:
             prev = slow;
             slow = next;
         }
-
-        // Step 3: Merge the two halves
         ListNode* firstHalf = head;
-        ListNode* secondHalf = prev; // prev now points to the head of the reversed second half
+        ListNode* secondHalf = prev;
         while (secondHalf->next) {
             next = firstHalf->next;
             prev = secondHalf->next;
