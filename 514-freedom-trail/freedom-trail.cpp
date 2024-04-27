15e831b58b6db6e1df48d71b30b8b89dec6d30f5
Vishistdeep <162116879+Vishistdeep@users.noreply.github.com>
2024-04-27 19:55:20 +0530

    Time: 18 ms (61.79%) | Memory: 11.2 MB (79.01%) - LeetSync

--- 514-freedom-trail/freedom-trail.cpp
+++ 514-freedom-trail/freedom-trail.cpp
@@ -9,32 +9,6 @@ public:
         for(int i=0;i<n;i++){
             arr[ring[i]-'a'].push_back(i);
         }
-        // q.push({0,-1,0});
-        // int s=1;
-        // int ans=INT_MAX;
-        // while(q.size()){
-        //     s=q.size();
-        //     while(s--){
-        //         auto ele=q.front();
-        //         q.pop();
-        //         int state=ele[0];
-        //         int ind_key=ele[1];
-        //         int steps=ele[2];
-        //         if(ind_key==m-1){
-        //             ans=min(ans,steps+m);
-        //             continue;
-        //         }
-        //         if(ind_key!=-1 && dp[state][ind_key]!=-1 && dp[state][ind_key]<steps) continue;
-        //         char c=key[ind_key+1];
-        //         for(int& i: arr[c-'a']){
-        //             int x=min(abs(i-state),abs(min(state,i)+n-max(state,i)));
-        //             if(dp[i][ind_key+1]!=-1 && dp[i][ind_key+1]<steps+x) continue;
-        //             dp[i][ind_key+1]=steps+x;
-        //             q.push({i,ind_key+1,steps+x});
-        //         }
-        //     }
-        // }
-        // return ans;
         vector<int> dp(n,0);
         for(int i=m-2;i>=0;i--){
             vector<int> vec;
